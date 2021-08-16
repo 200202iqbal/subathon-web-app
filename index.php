f853a4d6376465ac0f4d2392f0adf92395fe0e47
200202iqbal <200202yr@yse-c.net>
2021-08-16 19:25:17 +0900

    index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subhathon Web App</title>
+</head>
+<body>
+    <form action="">
+        <ul>
+            <h1>
+                Waktu Tersisa
+            </h1>
+            <h2> 00:00:00 </h2>
+            <li>
+                <input type="text" name="" id="">
+            </li>
+        </ul>
+    </form>
+</body>
+</html>
